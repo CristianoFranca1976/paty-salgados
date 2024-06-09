d75bb75e7e6e21b3b00366943cd43f0f35679403
CrstianoFranca1976 <cristiano.franca@hotmail.co.uk>
2024-06-09 15:34:02 +0100

    router

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: homedir()
+  }
+])
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
